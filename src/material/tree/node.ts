angular/components

fix(material/tree): add levelAccessor, childrenAccessor, TreeKeyManager; a11y and docs improvements
Update multiple facets of Tree component. Add APIs to manage data models, improve existing behaviors, add keyboard functionality and update documentation.

Add APIs options to the Tree data model by introducing levelAccessor and childrenAccessor. See “Api Addition” for usage. Currently, Tree component use TreeControl to manage data model. When applied, add levelAccessor and childrenAccessor functions as alternatives to TreeControl.

Add TreeKeyManager, which provides keyboard functionality. Currently Tree component allows developers to manage focus by setting tabindex on each tree node. When applied, Tree manages its own focus using key manager pattern. Keyboard commands match [WAI ARIA Tree View Pattern](https://www.w3.org/WAI/ARIA/apg/patterns/treeview/). See “Deprecated” for adopting changes to existing applications.

Correct the ARIA semantics of Tree and Tree node components.

Document updated APIs and behaviors. Refine documentation of existing APIs and behaviors.

Changes to Cdk Tree API also apply to Mat Tree API. See “Deprecated” for adopting changes to existing applications.

Accessibility:
 * add CdkTreeKeyManager to provide keyboard navigation for CdkTree and MatTree
 * Improve keyboard usability of CdkTreeNodeToggle.
 * Improve ARIA semantics of CdkTree, CdkTreeNode, Tree and TreeNode components
 * Fix miscellaneous accessibility issues in tree and cdk-tree examples
 * Add accessibility instructions to documentation

Documentation:
 * Add API and usage examples for TreeKeyManager
 * Update @angular/cdk/tree and @angular/material/tree to be more consistent
 * Update examples to use levelAccessor and childrenAccessor
 * Add example for (activation) on MatTreeNode and CdkTreeNode

API ADDITION: add CdkTree#childrenAccessor and CdkTree#levelAccessor
 * Add CdkTree#childrenAccessor. Given a data node, childrenAccessor determines the children of that node.
 * A

src/material/tree/node.ts
@@ -23,10 +23,18 @@ import {
   OnDestroy,
   OnInit,
 } from '@angular/core';
-import {CanDisable, HasTabIndex, mixinDisabled, mixinTabIndex} from '@angular/material/core';
-import {BooleanInput, coerceBooleanProperty} from '@angular/cdk/coercion';
+import {CanDisable, HasTabIndex} from '@angular/material/core';
+import {BooleanInput, coerceBooleanProperty, coerceNumberProperty} from '@angular/cdk/coercion';
+import {LegacyTreeKeyManager, TreeKeyManagerItem, TreeKeyManagerStrategy} from '@angular/cdk/a11y';
 
-const _MatTreeNodeBase = mixinTabIndex(mixinDisabled(CdkTreeNode));
+/**
+ * Determinte if argument TreeKeyManager is the LegacyTreeKeyManager. This function is safe to use with SSR.
+ */
+function isLegacyTreeKeyManager<T extends TreeKeyManagerItem>(
+  keyManager: TreeKeyManagerStrategy<T>,
+): keyManager is LegacyTreeKeyManager<T> {
+  return !!(keyManager as any)._isLegacyTreeKeyManager;
+}
 
 /**
  * Wrapper for the CdkTree node with Material design styles.
@@ -34,23 +42,87 @@ const _MatTreeNodeBase = mixinTabIndex(mixinDisabled(CdkTreeNode));
 @Directive({
   selector: 'mat-tree-node',
   exportAs: 'matTreeNode',
-  inputs: ['role', 'disabled', 'tabIndex'],
+  inputs: ['role', 'disabled', 'tabIndex', 'isExpandable', 'isExpanded', 'isDisabled'],
+  outputs: ['activation', 'expandedChange'],
   providers: [{provide: CdkTreeNode, useExisting: MatTreeNode}],
   host: {
     'class': 'mat-tree-node',
+    '[attr.aria-expanded]': '_getAriaExpanded()',
+    '[attr.aria-level]': 'level + 1',
+    '[attr.aria-posinset]': '_getPositionInSet()',
+    '[attr.aria-setsize]': '_getSetSize()',
+    '(click)': '_focusItem()',
+    'tabindex': '_getTabindexAttribute()',
   },
 })
 export class MatTreeNode<T, K = T>
-  extends _MatTreeNodeBase<T, K>
+  extends CdkTreeNode<T, K>
   implements CanDisable, HasTabIndex, OnInit, OnDestroy
 {
+  private _tabIndex: number;
+
+  /**
+   * The tabindex of the tree node.
+   *
+   * @deprecated By default MatTreeNode manages focus using TreeKeyManager instead of tabIndex.
+   *   Recommend to avoid setting tabIndex directly to prevent TreeKeyManager form getting into
+   *   an unexpected state. Tabindex to be removed in a future version.
+   * @breaking-change 19.0.0 Remove this attribute.
+   */
+  get tabIndex(): number {
+    return this.isDisabled ? -1 : this._tabIndex;
+  }
+  set tabIndex(value: number) {
+    // If the specified tabIndex value is null or undefined, fall back to the default value.
+    this._tabIndex = value != null ? coerceNumberProperty(value) : this.defaultTabIndex;
+  }
+
+  /**
+   * The default tabindex of the tree node.
+   *
+   * @deprecated By default MatTreeNode manages focus using TreeKeyManager instead of tabIndex.
+   *   Recommend to avoid setting tabIndex directly to prevent TreeKeyManager form getting into
+   *   an unexpected state. Tabindex to be removed in a future version.
+   * @breaking-change 19.0.0 Remove this attribute.
+   */
+  defaultTabIndex = 0;
+
+  protected _getTabindexAttribute() {
+    if (isLegacyTreeKeyManager(this._tree._keyManager)) {
+      return this.tabIndex;
+    }
+    return -1;
+  }
+
+  /**
+   * Whether the component is disabled.
+   *
+   * @deprecated This is an alias for `isDisabled`.
+   * @breaking-change 19.0.0 Remove this input
+   */
+  get disabled(): boolean {
+    return this.isDisabled ?? false;
+  }
+  set disabled(value: BooleanInput) {
+    this.isDisabled = coerceBooleanProperty(value);
+  }
+
   constructor(
     elementRef: ElementRef<HTMLElement>,
     tree: CdkTree<T, K>,
+    /**
+     * The tabindex of the tree node.
+     *
+     * @deprecated By default MatTreeNode manages focus using TreeKeyManager instead of tabIndex.
+     *   Recommend to avoid setting tabIndex directly to prevent TreeKeyManager form getting into
+     *   an unexpected state. Tabindex to be removed in a future version.
+     * @breaking-change 19.0.0 Remove this attribute.
+     */
     @Attribute('tabindex') tabIndex: string,
   ) {
     super(elementRef, tree);
-    this.tabIndex = Number(tabIndex) || 0;
+
+    this._tabIndex = Number(tabIndex) || this.defaultTabIndex;
   }
 
   // This is a workaround for https://github.com/angular/angular/issues/23091
@@ -83,7 +155,8 @@ export class MatTreeNodeDef<T> extends CdkTreeNodeDef<T> {
 @Directive({
   selector: 'mat-nested-tree-node',
   exportAs: 'matNestedTreeNode',
-  inputs: ['role', 'disabled', 'tabIndex'],
+  inputs: ['role', 'disabled', 'tabIndex', 'isExpandable', 'isExpanded', 'isDisabled'],
+  outputs: ['activation', 'expandedChange'],
   providers: [
     {provide: CdkNestedTreeNode, useExisting: MatNestedTreeNode},
     {provide: CdkTreeNode, useExisting: MatNestedTreeNode},
@@ -99,35 +172,29 @@ export class MatNestedTreeNode<T, K = T>
 {
   @Input('matNestedTreeNode') node: T;
 
-  /** Whether the node is disabled. */
-  @Input()
+  /**
+   * Whether the component is disabled.
+   *
+   * @deprecated This is an alias for `isDisabled`.
+   * @breaking-change 19.0.0 Remove this input
+   */
   get disabled(): boolean {
-    return this._disabled;
+    return this.isDisabled ?? false;
   }
   set disabled(value: BooleanInput) {
-    this._disabled = coerceBooleanProperty(value);
+    this.isDisabled = coerceBooleanProperty(value);
   }
-  private _disabled = false;
-
-  /** Tabindex for the node. */
-  @Input()
-  get tabIndex(): number {
-    return this.disabled ? -1 : this._tabIndex;
-  }
-  set tabIndex(value: number) {
-    // If the specified tabIndex value is null or undefined, fall back to the default value.
-    this._tabIndex = value != null ? value : 0;
-  }
-  private _tabIndex: number;
 
   constructor(
     elementRef: ElementRef<HTMLElement>,
     tree: CdkTree<T, K>,
     differs: IterableDiffers,
+    // Ignore tabindex attribute. MatTree manages its own active state using TreeKeyManager.
+    // Keeping tabIndex in constructor for backwards compatibility with trees created before
+    // introducing TreeKeyManager.
     @Attribute('tabindex') tabIndex: string,
   ) {
     super(elementRef, tree, differs);
-    this.tabIndex = Number(tabIndex) || 0;
   }
 
   // This is a workaround for https://github.com/angular/angular/issues/19145

Kxd395/angular

src/material/tree/node.ts
@@ -25,6 +25,18 @@ import {
   booleanAttribute,
   numberAttribute,
 } from '@angular/core';
+import {CanDisable, HasTabIndex} from '@angular/material/core';
+import {BooleanInput, coerceBooleanProperty, coerceNumberProperty} from '@angular/cdk/coercion';
+import {LegacyTreeKeyManager, TreeKeyManagerItem, TreeKeyManagerStrategy} from '@angular/cdk/a11y';
+
+/**
+ * Determinte if argument TreeKeyManager is the LegacyTreeKeyManager. This function is safe to use with SSR.
+ */
+function isLegacyTreeKeyManager<T extends TreeKeyManagerItem>(
+  keyManager: TreeKeyManagerStrategy<T>,
+): keyManager is LegacyTreeKeyManager<T> {
+  return !!(keyManager as any)._isLegacyTreeKeyManager;
+}
 
 /**
  * Wrapper for the CdkTree node with Material design styles.
@@ -32,30 +44,89 @@ import {
 @Directive({
   selector: 'mat-tree-node',
   exportAs: 'matTreeNode',
+  outputs: ['activation', 'expandedChange'],
   providers: [{provide: CdkTreeNode, useExisting: MatTreeNode}],
   host: {
     'class': 'mat-tree-node',
+    '[attr.aria-expanded]': '_getAriaExpanded()',
+    '[attr.aria-level]': 'level + 1',
+    '[attr.aria-posinset]': '_getPositionInSet()',
+    '[attr.aria-setsize]': '_getSetSize()',
+    '(click)': '_focusItem()',
+    'tabindex': '_getTabindexAttribute()',
   },
   standalone: true,
 })
 export class MatTreeNode<T, K = T> extends CdkTreeNode<T, K> implements OnInit, OnDestroy {
-  /** Whether the node is disabled. */
-  @Input({transform: booleanAttribute})
-  disabled: boolean = false;
-
   /** Tabindex of the node. */
   @Input({
     transform: (value: unknown) => (value == null ? 0 : numberAttribute(value)),
   })
-  tabIndex: number;
+  get tabIndex(): number {
+    return this.isDisabled ? -1 : this._tabIndex;
+  }
+  set tabIndex(value: number) {
+    // If the specified tabIndex value is null or undefined, fall back to the default value.
+    this._tabIndex = value;
+  }
+  private _tabIndex: number;
+
+  /**
+   * The tabindex of the tree node.
+   *
+   * @deprecated By default MatTreeNode manages focus using TreeKeyManager instead of tabIndex.
+   *   Recommend to avoid setting tabIndex directly to prevent TreeKeyManager form getting into
+   *   an unexpected state. Tabindex to be removed in a future version.
+   * @breaking-change 19.0.0 Remove this attribute.
+   */
+
+  /**
+   * The default tabindex of the tree node.
+   *
+   * @deprecated By default MatTreeNode manages focus using TreeKeyManager instead of tabIndex.
+   *   Recommend to avoid setting tabIndex directly to prevent TreeKeyManager form getting into
+   *   an unexpected state. Tabindex to be removed in a future version.
+   * @breaking-change 19.0.0 Remove this attribute.
+   */
+  defaultTabIndex = 0;
+
+  protected _getTabindexAttribute() {
+    if (isLegacyTreeKeyManager(this._tree._keyManager)) {
+      return this.tabIndex;
+    }
+    return -1;
+  }
+
+  /**
+   * Whether the component is disabled.
+   *
+   * @deprecated This is an alias for `isDisabled`.
+   * @breaking-change 19.0.0 Remove this input
+   */
+  @Input({transform: booleanAttribute})
+  get disabled(): boolean {
+    return this.isDisabled;
+  }
+  set disabled(value: boolean) {
+    this.isDisabled = value;
+  }
 
   constructor(
     elementRef: ElementRef<HTMLElement>,
     tree: CdkTree<T, K>,
+    /**
+     * The tabindex of the tree node.
+     *
+     * @deprecated By default MatTreeNode manages focus using TreeKeyManager instead of tabIndex.
+     *   Recommend to avoid setting tabIndex directly to prevent TreeKeyManager form getting into
+     *   an unexpected state. Tabindex to be removed in a future version.
+     * @breaking-change 19.0.0 Remove this attribute.
+     */
     @Attribute('tabindex') tabIndex: string,
   ) {
     super(elementRef, tree);
-    this.tabIndex = Number(tabIndex) || 0;
+
+    this.tabIndex = Number(tabIndex) || this.defaultTabIndex;
   }
 
   // This is a workaround for https://github.com/angular/angular/issues/23091
@@ -89,6 +160,7 @@ export class MatTreeNodeDef<T> extends CdkTreeNodeDef<T> {
 @Directive({
   selector: 'mat-nested-tree-node',
   exportAs: 'matNestedTreeNode',
+  outputs: ['activation', 'expandedChange'],
   providers: [
     {provide: CdkNestedTreeNode, useExisting: MatNestedTreeNode},
     {provide: CdkTreeNode, useExisting: MatNestedTreeNode},
@@ -105,18 +177,30 @@ export class MatNestedTreeNode<T, K = T>
 {
   @Input('matNestedTreeNode') node: T;
 
-  /** Whether the node is disabled. */
+  /**
+   * Whether the node is disabled.
+   *
+   * @deprecated This is an alias for `isDisabled`.
+   * @breaking-change 19.0.0 Remove this input
+   */
   @Input({transform: booleanAttribute})
-  disabled: boolean = false;
+  get disabled(): boolean {
+    return this.isDisabled;
+  }
+  set disabled(value: boolean) {
+    this.isDisabled = value;
+  }
 
-  /** Tabindex for the node. */
-  @Input()
+  /** Tabindex of the node. */
+  @Input({
+    transform: (value: unknown) => (value == null ? 0 : numberAttribute(value)),
+  })
   get tabIndex(): number {
-    return this.disabled ? -1 : this._tabIndex;
+    return this.isDisabled ? -1 : this._tabIndex;
   }
   set tabIndex(value: number) {
     // If the specified tabIndex value is null or undefined, fall back to the default value.
-    this._tabIndex = value != null ? value : 0;
+    this._tabIndex = value;
   }
   private _tabIndex: number;
 
@@ -124,10 +208,12 @@ export class MatNestedTreeNode<T, K = T>
     elementRef: ElementRef<HTMLElement>,
     tree: CdkTree<T, K>,
     differs: IterableDiffers,
+    // Ignore tabindex attribute. MatTree manages its own active state using TreeKeyManager.
+    // Keeping tabIndex in constructor for backwards compatibility with trees created before
+    // introducing TreeKeyManager.
     @Attribute('tabindex') tabIndex: string,
   ) {
     super(elementRef, tree, differs);
-    this.tabIndex = Number(tabIndex) || 0;
   }
 
   // This is a workaround for https://github.com/angular/angular/issues/19145